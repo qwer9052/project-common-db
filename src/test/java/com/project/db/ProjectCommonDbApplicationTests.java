ecedf30f2dca8be40625a2b4196369781694cfaa
qwer9052 <qwer9052@gmail.com>
2023-10-20 20:28:34 +0900

    init

---
+++ src/test/java/com/project/db/ProjectCommonDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCommonDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
